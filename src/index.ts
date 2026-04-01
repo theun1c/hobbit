b83641ba9f74d1285be98810ff44ac58ea3a5617
theun1c <belovdima1122@yandex.ru>
2026-04-01 22:16:13 +0300

    feat: start impl handlers

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,54 @@
 import 'dotenv/config'
 import http from 'http'
 
+const PORT = Number(process.env.PORT) || 5000
+
 const server = http.createServer((req, res) => {
     res.writeHead( 200, {
         'Content-type': 'text/html; charset=UTF-8' 
     })
 
     if(req.url === '/health'){
-        return res.end('HEALTH PAGE')
+        return HealthHandler(req, res)
     }
 
     if(req.url === '/habits'){
-        return res.end('HABITS PAGE')
+        return HabitHandler(req, res)
     }
 
     res.end('/')
 });
 
-const PORT = process.env.PORT || 5000
-
 server.listen(PORT, () => {
     console.log(`start on port: ${PORT}`)
 })
+
+function HealthHandler(req:http.IncomingMessage , res: http.ServerResponse<http.IncomingMessage>) {
+    
+    res.writeHead( 200, {
+        'Content-type': 'text/html; charset=UTF-8' 
+    })
+
+    if (req.method === "GET"){
+        return res.end('HEALTH PAGE --- GET')
+    }
+
+    if (req.method === "POST"){
+        return res.end('HEALTH PAGE --- POST')
+    }
+}
+
+function HabitHandler(req:http.IncomingMessage , res: http.ServerResponse<http.IncomingMessage>) {
+    
+    res.writeHead( 200, {
+        'Content-type': 'text/html; charset=UTF-8' 
+    })
+
+    if (req.method === "GET"){
+        return res.end('HABIT PAGE --- GET')
+    }
+
+    if (req.method === "POST"){
+        return res.end('HABIT PAGE --- POST')
+    }
+}
