89a3ab9e01ca9b2f0167abf95b1d624d1c72b396
theun1c <belovdima1122@yandex.ru>
2026-04-01 19:58:37 +0300

    feat: create base routing and practice with env

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
+import 'dotenv/config'
 import http from 'http'
 
 const server = http.createServer((req, res) => {
-    // res.statusCode = 200 
-    // res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello world');
+    res.writeHead( 200, {
+        'Content-type': 'text/html; charset=UTF-8' 
+    })
+
+    if(req.url === '/health'){
+        return res.end('HEALTH PAGE')
+    }
+
+    if(req.url === '/habits'){
+        return res.end('HABITS PAGE')
+    }
+
+    res.end('/')
 });
 
-const PORT = 3000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
-    console.log("start")
+    console.log(`start on port: ${PORT}`)
 })
